7d1e406f64826dd06de49f8e1cd9ce618ef0b454
Pablo <pmahuzieru@gmail.com>
2025-01-23 00:08:38 -0300

    Initial migration - database setup

--- sistema_reportes_ppda/sistema_reportes_ppda/settings.py
+++ sistema_reportes_ppda/sistema_reportes_ppda/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+# Initialize environ
+env = environ.Env()
+
+# Se debe crear un archivo .env (en este caso .env.dev) especificando la DATABASE_URL
+environ.Env.read_env(env_file='.env.dev')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,10 +81,7 @@ WSGI_APPLICATION = 'sistema_reportes_ppda.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db('DATABASE_URL')
 }
 
 
