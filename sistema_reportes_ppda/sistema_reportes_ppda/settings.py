3665c0f82eed105961a6c58d39fc15531fc3d178
rypptc <rypptc@gmail.com>
2025-01-25 22:22:23 +0100

    install drf and update settings

--- sistema_reportes_ppda/sistema_reportes_ppda/settings.py
+++ sistema_reportes_ppda/sistema_reportes_ppda/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +131,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Custom User setting
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+
+# DRF
+REST_FRAMEWORK = {
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
