21a79da266bee60d382bd250d51f8a76b6fea082
MI Strobel <ma.ignaciasb@gmail.com>
2025-02-02 19:42:19 -0300

    Agrega readme para uso de swagger

--- sistema_reportes_ppda/sistema_reportes_ppda/settings.py
+++ sistema_reportes_ppda/sistema_reportes_ppda/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-aghi@h_4c+s0!7v*^n6h^xlp#(ftm-6ge!xr6nt9knn4!cj1)p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
