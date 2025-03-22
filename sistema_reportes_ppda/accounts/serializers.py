3a1534e0229a3ef55d3bf0ac1287b63214c47c38
Pablo <pmahuzieru@gmail.com>
2025-03-22 01:34:36 -0300

    Add register User endpoint

--- sistema_reportes_ppda/accounts/serializers.py
+++ sistema_reportes_ppda/accounts/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
 from accounts.models import CustomUser
 from accounts.utils import validate_rut
+from management.models import Body
 
 class CustomUserSerializer(serializers.ModelSerializer):
     # Evitar que se exponga el password
@@ -10,10 +11,16 @@ class CustomUserSerializer(serializers.ModelSerializer):
         required=True,
         validators=[validate_password]
     )
+    
+    email = serializers.EmailField(required=True)
+    rut = serializers.CharField(required=True)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+    body = serializers.PrimaryKeyRelatedField(queryset=Body.objects.all())
 
     class Meta:
         model = CustomUser
-        fields = ["id", "username", "password", "email", "rut", "first_name", "last_name", "is_active"]
+        fields = ["id", "username", "password", "email", "rut", "first_name", "last_name", "is_active", "body"]
 
     def create(self, validated_data):
         """Crea un usuario con la contraseña hasheada automáticamente."""
