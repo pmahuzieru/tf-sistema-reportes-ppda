fc63fc09b1db688a8a3425106feabfc97b374c97
Pablo <pmahuzieru@gmail.com>
2025-03-22 12:15:58 -0300

    Format document

--- sistema_reportes_ppda/management/models.py
+++ sistema_reportes_ppda/management/models.py
@@ -1,94 +1,203 @@
 from django.db import models
 
+
 class EnvironmentalPlan(models.Model):
     PLAN_TYPE_CHOICES = [  # https://ppda.mma.gob.cl/
         ("PPA", "Plan de Prevención Atmosférica (PPA)"),
         ("PDA", "Plan de Descontaminación Atmosférica (PDA)"),
-        ("PPDA", "Plan de Prevención y Descontaminación Atmosférica (PPDA)")
+        ("PPDA", "Plan de Prevención y Descontaminación Atmosférica (PPDA)"),
     ]
-    
+
     name = models.CharField(max_length=200, null=False, blank=False)
     short_name = models.CharField(max_length=50, null=False, blank=False)
-    type = models.CharField(max_length=4, choices=PLAN_TYPE_CHOICES, default="PPDA", null=False, blank=False)
+    type = models.CharField(
+        max_length=4, choices=PLAN_TYPE_CHOICES, default="PPDA", null=False, blank=False
+    )
     created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey('accounts.CustomUser', on_delete=models.PROTECT, related_name="created_plans", null=False, blank=False)
+    created_by = models.ForeignKey(
+        "accounts.CustomUser",
+        on_delete=models.PROTECT,
+        related_name="created_plans",
+        null=False,
+        blank=False,
+    )
     updated_at = models.DateTimeField(auto_now=True)
-    updated_by = models.ForeignKey('accounts.CustomUser', on_delete=models.PROTECT, related_name='updated_plans', null=True, blank=True)
-    
+    updated_by = models.ForeignKey(
+        "accounts.CustomUser",
+        on_delete=models.PROTECT,
+        related_name="updated_plans",
+        null=True,
+        blank=True,
+    )
+
     def __str__(self):
         return self.short_name
 
 
 class Measure(models.Model):
-    MEASURE_TYPE_CHOICES = [ 
-        ('NA', "No Aplica"),
-        ('PP', "Politica Publica"),
-        ('EyD', "Educacion y Difusion"),
-        ('E', "Estudios"),
-        ('O', "Otra")
+    MEASURE_TYPE_CHOICES = [
+        ("NA", "No Aplica"),
+        ("PP", "Politica Publica"),
+        ("EyD", "Educacion y Difusion"),
+        ("E", "Estudios"),
+        ("O", "Otra"),
     ]
 
-    reference_PDA = models.ForeignKey(EnvironmentalPlan, on_delete=models.PROTECT, related_name='measures', null=False, blank=False)
-    measure_type = models.CharField(max_length=4, choices=MEASURE_TYPE_CHOICES, default=None, blank=True)
+    reference_PDA = models.ForeignKey(
+        EnvironmentalPlan,
+        on_delete=models.PROTECT,
+        related_name="measures",
+        null=False,
+        blank=False,
+    )
+    measure_type = models.CharField(
+        max_length=4, choices=MEASURE_TYPE_CHOICES, default=None, blank=True
+    )
     short_name = models.CharField(max_length=500, null=False, blank=False)
     indicator = models.CharField(max_length=500, null=False, blank=False)
     calculation_formula = models.CharField(max_length=500, null=False, blank=False)
     reporting_frequency = models.CharField(max_length=50, null=False, blank=False)
     verification_methods = models.CharField(max_length=500, null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey('accounts.CustomUser', on_delete=models.PROTECT, related_name="created_measures", null=False, blank=False)
+    created_by = models.ForeignKey(
+        "accounts.CustomUser",
+        on_delete=models.PROTECT,
+        related_name="created_measures",
+        null=False,
+        blank=False,
+    )
     updated_at = models.DateTimeField(auto_now=True)
-    updated_by = models.ForeignKey('accounts.CustomUser', on_delete=models.PROTECT, related_name='updated_measures', null=True, blank=True)
+    updated_by = models.ForeignKey(
+        "accounts.CustomUser",
+        on_delete=models.PROTECT,
+        related_name="updated_measures",
+        null=True,
+        blank=True,
+    )
     is_regulatory = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.reference_PDA.short_name} - {self.short_name}"
-    
+
 
 class MeasureReport(models.Model):
-    measure = models.ForeignKey(Measure, on_delete=models.CASCADE, related_name="measure_reports", null=False, blank=False)
+    measure = models.ForeignKey(
+        Measure,
+        on_delete=models.CASCADE,
+        related_name="measure_reports",
+        null=False,
+        blank=False,
+    )
     reported_value = models.CharField(max_length=20, null=False, blank=False)
-    
+
     created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey('accounts.CustomUser', on_delete=models.PROTECT, related_name="created_measure_reports", null=False, blank=False)
+    created_by = models.ForeignKey(
+        "accounts.CustomUser",
+        on_delete=models.PROTECT,
+        related_name="created_measure_reports",
+        null=False,
+        blank=False,
+    )
     updated_at = models.DateTimeField(auto_now=True)
-    updated_by = models.ForeignKey('accounts.CustomUser', on_delete=models.PROTECT, related_name='updated_measure_reports', null=True, blank=True)
-    
+    updated_by = models.ForeignKey(
+        "accounts.CustomUser",
+        on_delete=models.PROTECT,
+        related_name="updated_measure_reports",
+        null=True,
+        blank=True,
+    )
+
     def __str__(self):
         return f"{self.measure.reference_PDA.short_name} - {self.measure.id} - {self.created_at.strftime('%Y-%m-%d %H:%M:%S')}"
-    
+
 
 class ReportFile(models.Model):
-    report = models.ForeignKey(MeasureReport, on_delete=models.CASCADE, related_name="files", null=False, blank=False)
+    report = models.ForeignKey(
+        MeasureReport,
+        on_delete=models.CASCADE,
+        related_name="files",
+        null=False,
+        blank=False,
+    )
     description = models.CharField(max_length=255, null=False, blank=False)
-    file = models.FileField(upload_to='uploads/')    
+    file = models.FileField(upload_to="uploads/")
     created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey('accounts.CustomUser', on_delete=models.PROTECT, related_name="created_report_files", null=False, blank=False)
+    created_by = models.ForeignKey(
+        "accounts.CustomUser",
+        on_delete=models.PROTECT,
+        related_name="created_report_files",
+        null=False,
+        blank=False,
+    )
     updated_at = models.DateTimeField(auto_now=True)
-    updated_by = models.ForeignKey('accounts.CustomUser', on_delete=models.PROTECT, related_name='updated_report_files', null=True, blank=True)
-    
+    updated_by = models.ForeignKey(
+        "accounts.CustomUser",
+        on_delete=models.PROTECT,
+        related_name="updated_report_files",
+        null=True,
+        blank=True,
+    )
+
     def __str__(self):
         return f"{self.description}"
+
 
 class Body(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey('accounts.CustomUser', on_delete=models.PROTECT, related_name="created_bodies", null=False, blank=False)
+    created_by = models.ForeignKey(
+        "accounts.CustomUser",
+        on_delete=models.PROTECT,
+        related_name="created_bodies",
+        null=False,
+        blank=False,
+    )
     updated_at = models.DateTimeField(auto_now=True)
-    updated_by = models.ForeignKey('accounts.CustomUser', on_delete=models.PROTECT, related_name='updated_bodies', null=True, blank=True)
+    updated_by = models.ForeignKey(
+        "accounts.CustomUser",
+        on_delete=models.PROTECT,
+        related_name="updated_bodies",
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return self.name
 
+
 class BodyMeasure(models.Model):
-    fk_measure = models.ForeignKey(Measure, on_delete=models.CASCADE, related_name="measure_bodymeasure", null=False, blank=False)
-    fk_body = models.ForeignKey(Body, on_delete=models.CASCADE, related_name="body_bodymeasure", null=False, blank=False)
+    fk_measure = models.ForeignKey(
+        Measure,
+        on_delete=models.CASCADE,
+        related_name="measure_bodymeasure",
+        null=False,
+        blank=False,
+    )
+    fk_body = models.ForeignKey(
+        Body,
+        on_delete=models.CASCADE,
+        related_name="body_bodymeasure",
+        null=False,
+        blank=False,
+    )
     is_reporter = models.BooleanField(default=True)
-    active = models.BooleanField(default=True)    
+    active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey('accounts.CustomUser', on_delete=models.PROTECT, related_name="created_bodymeasures", null=False, blank=False)
+    created_by = models.ForeignKey(
+        "accounts.CustomUser",
+        on_delete=models.PROTECT,
+        related_name="created_bodymeasures",
+        null=False,
+        blank=False,
+    )
     updated_at = models.DateTimeField(auto_now=True)
-    updated_by = models.ForeignKey('accounts.CustomUser', on_delete=models.PROTECT, related_name='updated_bodymeasures', null=True, blank=True)
+    updated_by = models.ForeignKey(
+        "accounts.CustomUser",
+        on_delete=models.PROTECT,
+        related_name="updated_bodymeasures",
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return f"{self.fk_body.name} - {self.fk_measure.short_name}"
