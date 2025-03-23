467db73d304288811ee1ac68a1711a41149da20d
Pablo <pmahuzieru@gmail.com>
2025-03-23 12:50:59 -0300

    Add ProgressReport model to app and admin

--- sistema_reportes_ppda/reporting/admin.py
+++ sistema_reportes_ppda/reporting/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from reporting.models import ProgressReport
 
-# Register your models here.
+@admin.register(ProgressReport)
+class ProgressReportAdmin(admin.ModelAdmin):
+    list_display = ('name', 'environmental_plan', 'created_at')
+    search_fields = ('name',)    
+    readonly_fields = ('created_at', 'created_by', 'updated_at',  'updated_by')
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.created_by = request.user
+        
+        obj.updated_by = request.user
+        super().save_model(request, obj, form, change)
--- sistema_reportes_ppda/reporting/models.py
+++ sistema_reportes_ppda/reporting/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+from management.models import EnvironmentalPlan
+
 # Create your models here.
+class ProgressReport(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    environmental_plan = models.ForeignKey(EnvironmentalPlan, on_delete=models.PROTECT, related_name="progress_reports")
+    publication_date = models.DateField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        "accounts.CustomUser",
+        on_delete=models.PROTECT,
+        related_name="created_progress_reports",
+        null=False,
+        blank=False,
+    )
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(
+        "accounts.CustomUser",
+        on_delete=models.PROTECT,
+        related_name="updated_progress_reports",
+        null=True,
+        blank=True,
+    )
