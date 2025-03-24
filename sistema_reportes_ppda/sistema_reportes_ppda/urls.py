10b2f870a2b63f446564f87eb03a025c049e573b
Pablo <pmahuzieru@gmail.com>
2025-03-24 20:53:18 -0300

    Configure static files path

--- sistema_reportes_ppda/sistema_reportes_ppda/urls.py
+++ sistema_reportes_ppda/sistema_reportes_ppda/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from sistema_reportes_ppda.settings import DEBUG
+from sistema_reportes_ppda.settings import DEBUG, STATIC_URL, STATIC_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,3 +33,5 @@ if DEBUG:
         path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
         path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     ]
+    
+    urlpatterns += static(STATIC_URL, document_root=STATIC_ROOT)
