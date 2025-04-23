69832999c841f1f7b32d1eaf579df5070877f9e1
Pablo <pmahuzieru@gmail.com>
2025-04-22 20:25:35 -0400

    add blank line

--- sistema_reportes_ppda/accounts/models.py
+++ sistema_reportes_ppda/accounts/models.py
@@ -7,6 +7,7 @@ class CustomUser(AbstractUser):
     Represents a system user representing a natural person (RUT) that belongs to 
     a sectorial body.
     """
+    
     rut = models.CharField(max_length=12, unique=True, blank=True, null=True)
     body = models.ForeignKey(Body, on_delete=models.CASCADE, related_name='body_users', null=True, blank=True)
 
