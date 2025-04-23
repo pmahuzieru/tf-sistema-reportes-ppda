a6cc4efa7b723f86cfcd6e5453d6969fd70664be
Pablo <pmahuzieru@gmail.com>
2025-04-22 20:24:45 -0400

    Docstring account app models

--- sistema_reportes_ppda/accounts/models.py
+++ sistema_reportes_ppda/accounts/models.py
@@ -3,6 +3,10 @@ from django.db import models
 from management.models import Body
 
 class CustomUser(AbstractUser):
+    """
+    Represents a system user representing a natural person (RUT) that belongs to 
+    a sectorial body.
+    """
     rut = models.CharField(max_length=12, unique=True, blank=True, null=True)
     body = models.ForeignKey(Body, on_delete=models.CASCADE, related_name='body_users', null=True, blank=True)
 
