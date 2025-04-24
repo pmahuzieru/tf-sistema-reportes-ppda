c4a08307ab24136928fd6676b7d24c19204ad9e7
Pablo <pmahuzieru@gmail.com>
2025-04-24 13:58:29 -0400

    Remove pwd change

--- build.sh
+++ build.sh
@@ -6,9 +6,6 @@ set -o errexit
 # Entrar a la carpeta del proyecto Django
 cd sistema_reportes_ppda
 
-# Asegura que Python vea este directorio como parte del PYTHONPATH
-export PYTHONPATH=$PYTHONPATH:$(pwd)
-
 # Instala las dependencias
 pip install --upgrade pip
 pip install -r ../requirements.txt
