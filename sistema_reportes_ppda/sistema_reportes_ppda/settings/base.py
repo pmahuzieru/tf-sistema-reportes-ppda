9a138e001c5bab92c55419f9ba5c8bb90ac281b7
Pablo <pmahuzieru@gmail.com>
2025-04-23 22:53:09 -0400

    delete print env path

--- sistema_reportes_ppda/sistema_reportes_ppda/settings/base.py
+++ sistema_reportes_ppda/sistema_reportes_ppda/settings/base.py
@@ -21,8 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
 environ.Env.read_env(env_file=os.getenv("ENV_FILE", ".env"))
 
-print("ENV PATH:", os.getenv("ENV_FILE", ".env"))
-
 SECRET_KEY = env("SECRET_KEY")
 
 DEBUG = env.bool("DEBUG", default=False)
